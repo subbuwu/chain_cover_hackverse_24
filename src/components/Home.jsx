7df17d3d4011e3556eb674411a5aee721395dbd3
subbbuwu <nsubbu2004@gmail.com>
2024-04-07 02:40:46 +0530

    added form modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { ethers, formatEther } from 'ethers';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/store';
-import { HoverBorderGradient } from "./ui/hover-button.jsx";
-import { Button } from './ui/Button.jsx';
 import toast from 'react-hot-toast';
 
 const Home = () => {
@@ -108,6 +106,7 @@ const Home = () => {
           </span>
           <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-4 px-8 ring-1 ring-white/10 ">
             <span>
+              Go To Dashboard
             </span>
             <svg
               fill="none"
--- src/components/ui/BentoGridItem.jsx
+++ src/components/ui/BentoGridItem.jsx
@@ -27,7 +27,7 @@ export const BentoGridItem = ({ imgSrc, title,desc, attachedFunction }) => {
           {desc}
         </div>
         <div className="w-full mt-1 flex">
-          <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block" onClick={()=>attachedFunction}>
+          <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block" onClick={attachedFunction}>
             <span className="absolute inset-0 overflow-hidden rounded-full">
               <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
             </span>
