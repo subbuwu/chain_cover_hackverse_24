9163fa56f13401ef60b4586c74f6683ad1f75243
subbbuwu <nsubbu2004@gmail.com>
2024-04-07 04:35:14 +0530

    buy part done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,62 @@
 import React from "react";
 import "../font.css";
+import { useAuthStore } from "../store/store";
+import { useEffect } from "react";
+import { useState } from "react";
+import { ethers } from "ethers";
+import { abi } from "../../artifacts/InsuranceContractABI";
 
 const Navbar = () => {
+
+  useEffect(()=>{
+    accessContract()
+  },[])
+
+
+  const contractAddress = "0xb00f410ee2c695917fa891c1184256c4b977fc96";
+
+  const { policies,provider } = useAuthStore();
+  const setPolicies = useAuthStore((state) => state.updatePolicies);
+  const [providerContract,setProviderContract] = useState();
+
+  const accessContract = async () => {
+    try {
+      if (window.ethereum) {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const contract = new ethers.Contract(contractAddress, abi, provider);
+        setProviderContract(contract);
+      } else {
+        console.error("MetaMask is not installed or not connected");
+      }
+    } catch (error) {
+      console.error("Error accessing contract:", error);
+    }
+  };
+
+  const getUserPolicies = async () => {
+    try {
+      if (providerContract) {
+        // If providerContract is defined, proceed with calling methods
+        const gettingPolicies = await providerContract.getPolicies();
+        console.log(gettingPolicies);
+  
+        const getPoliciesString = gettingPolicies.toString();
+        console.log("Policies:", getPoliciesString);
+  
+        setPolicies(gettingPolicies);
+        console.log(policies);
+      } else {
+        console.error("Provider contract is undefined");
+      }
+    } catch (error) {
+      console.error("Error calling getPolicies :", error);
+    }
+  };
+  
+
   return (
-    <header
-      className="z-[50] fixed top-0 w-full border-b  backdrop-blur-sm bg-white/[0.6] dark:bg-black/[0.6] border-zinc-700"
-    >
+    <header className="z-[50] fixed top-0 w-full border-b  backdrop-blur-sm bg-white/[0.6] dark:bg-black/[0.6] border-zinc-700">
       <div className="p-[2rem] md:p-0 flex h-16 items-center max-w-[88rem] mx-auto">
         <div className="mr-4 hidden md:flex">
           <a
@@ -50,7 +101,6 @@ const Navbar = () => {
                 height="50"
                 decoding="async"
                 data-nimg="1"
-
                 src="./link-hyperlink-color-icon.svg"
               />
             </div>
@@ -58,7 +108,20 @@ const Navbar = () => {
           <span className="ml-2 text-white text-2xl">Chain Cover</span>
         </a>
         <div className="flex flex-1 items-center justify-end gap-2 sm:gap-2 md:justify-end">
-        <button className="sm:flex relative hidden justify-start items-center text-sm text-muted-foreground dark:border-white/[0.2] py-2 w-fit border border-transparent shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] px-4 rounded-xl bg-white dark:bg-brand"><span className="transition-colors hover:text-foreground/80 text-foreground/60 text-sm font-medium pl-2 pr-4">Contact Us</span><kbd className="pointer-events-none  hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex"><span className="text-xs">⌘</span>K</kbd></button>
+          <button className="p-[3px] relative mr-4" onClick={()=>getUserPolicies()}>
+            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+            <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+              My Policies
+            </div>
+          </button>
+          <button className="sm:flex relative hidden justify-start items-center text-sm text-muted-foreground dark:border-white/[0.2] py-2 w-fit border border-transparent shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] px-4 rounded-xl bg-white dark:bg-brand">
+            <span className="transition-colors hover:text-foreground/80 text-foreground/60 text-sm font-medium pl-2 pr-4">
+              Contact Us
+            </span>
+            <kbd className="pointer-events-none  hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+              <span className="text-xs">⌘</span>K
+            </kbd>
+          </button>
         </div>
       </div>
     </header>
