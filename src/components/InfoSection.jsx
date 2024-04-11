b9589da0b244d5b682149b6cf1bf0c556529cbf7
Subramanian Narayanan <101008478+subbuwu@users.noreply.github.com>
2024-04-11 23:29:11 +0530

    Update InfoSection.jsx

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -3,7 +3,7 @@ import {MeteorCard} from "./MeteorCard.jsx"
 
 const InfoSection = () => {
   return (
-    <section className='text-white px-10 sm:mt-40 mt-28 gap-4 items-center justify-center flex sm:flex-row flex-col'>
+    <section className='text-white px-10 sm:mt-40 mt-28 mb-10 gap-4 items-center justify-center flex sm:flex-row flex-col'>
         <MeteorCard title="Future-Proof Insurance: Transparent & Secure" desc= "Explore our innovative insurance platform for peace of mind. Backed by blockchain, it's insurance reimagined."/>
         <MeteorCard title="Trust in Tomorrow: Insurance Reinvented" desc= "Step into the future of insurance with our revolutionary platform. It's insurance you can rely on."/>
     </section>
